b0752a526c10b16d429a54413b6252c4fedd33e0
Yaroslav Kuchkovsky <y.kuchkovsky@gmail.com>
2018-04-13 13:41:37 +0300

    Initial project

---
+++ src/main/java/com/softserve/edu/hypercinema/HyperCinemaApplication.java
@@ -0,0 +1,12 @@
+package com.softserve.edu.hypercinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyperCinemaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyperCinemaApplication.class, args);
+    }
+}
