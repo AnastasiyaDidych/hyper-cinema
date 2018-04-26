03f63439ed45a912c25c04f25e82dcf394210df7
Yaroslav Kuchkovsky <y.kuchkovsky@gmail.com>
2018-04-26 21:36:17 +0300

    Fix Swagger

--- src/main/java/com/softserve/edu/hypercinema/config/WebSecurityConfig.java
+++ src/main/java/com/softserve/edu/hypercinema/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -48,6 +49,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.addFilter(new JwtAuthorizationFilter(authenticationManager(), secretKey, userDetailsService))
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+	}
+
+	@Override
+	public void configure(WebSecurity web) {
+		web.ignoring().antMatchers("/swagger-ui.html", "/swagger-resources/**", "/webjars/**", "/v2/api-docs");
 	}
 
 	@Override
