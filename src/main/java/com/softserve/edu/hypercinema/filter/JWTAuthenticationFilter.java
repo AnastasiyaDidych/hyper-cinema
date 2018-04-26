616fa20f261a9fd5dff18047dbf7f6f8efa93ddc
VR-Java <vitaliy.rolsky@gmail.com>
2018-04-26 20:56:29 +0300

    db_config modified according ti new DB structure

--- src/main/java/com/softserve/edu/hypercinema/filter/JWTAuthenticationFilter.java
+++ src/main/java/com/softserve/edu/hypercinema/filter/JWTAuthenticationFilter.java
@@ -1,79 +1,73 @@
 package com.softserve.edu.hypercinema.filter;
 
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.databind.DeserializationFeature;
+import org.springframework.security.authentication.BadCredentialsException;
+import com.softserve.edu.hypercinema.dto.UserCredentialsDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.softserve.edu.hypercinema.dto.UserDto;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.crypto.SecretKey;
 import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.stream.Collectors;
 
-import static com.softserve.edu.hypercinema.constants.SecurityConstants.*;
+import static com.softserve.edu.hypercinema.constants.JwtConstants.EXPIRATION_TIME;
+import static com.softserve.edu.hypercinema.constants.JwtConstants.HEADER_STRING;
+import static com.softserve.edu.hypercinema.constants.JwtConstants.TOKEN_PREFIX;
 
-public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private static final String CREDENTIALS_READING_ERROR_MESSAGE = "Can not read credentials";
+	private static final String CREDENTIALS_READ_ERROR_MESSAGE = "Failed to read credentials";
 
-    private AuthenticationManager authenticationManager;
+	private AuthenticationManager authenticationManager;
 
-    public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
-        this.authenticationManager = authenticationManager;
-        this.setFilterProcessesUrl("/api/login");
-    }
+	private SecretKey secretKey;
 
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException {
-        try {
-            UserDto userDto = new ObjectMapper().readValue(request.getInputStream(), UserDto.class);
-            return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
-                    userDto.getEmail(),
-                    userDto.getPassword(),
-                    Collections.emptyList()));
-        } catch (IOException e) {
-            //TODO prepare special Exception
-            e.printStackTrace();
-            throw new RuntimeException(CREDENTIALS_READING_ERROR_MESSAGE);
-        }
-    }
+	public JwtAuthenticationFilter(AuthenticationManager authenticationManager, SecretKey secretKey) {
+		this.authenticationManager = authenticationManager;
+		this.secretKey = secretKey;
+		this.setFilterProcessesUrl("/auth/login");
+	}
 
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
-        // get username from Authentication
-        // in our case it is email
-        String email = ((User) authResult.getPrincipal()).getUsername();
-        //get list of ROLES
-        Claims claims = Jwts.claims().setSubject(email);
-        claims.put("roles", authResult.getAuthorities()
-                .stream().map(Object::toString).collect(Collectors.toList()));
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException {
+		try {
+			UserCredentialsDto credentials = new ObjectMapper()
+					.readValue(request.getInputStream(), UserCredentialsDto.class);
+			return authenticationManager.authenticate(
+					new UsernamePasswordAuthenticationToken(
+							credentials.getEmail(),
+							credentials.getPassword(),
+							new ArrayList<>())
+			);
+		} catch (IOException e) {
+			throw new BadCredentialsException(CREDENTIALS_READ_ERROR_MESSAGE);
+		}
+	}
 
-        String token = Jwts.builder()
-                .setClaims(claims)
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
-                .signWith(SignatureAlgorithm.HS512, SECRET.getBytes())
-                .compact();
+	@Override
+	protected void successfulAuthentication(HttpServletRequest req, HttpServletResponse res, FilterChain chain,
+											Authentication auth) {
+		String username = ((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername();
+		Claims claims = Jwts.claims().setSubject(username);
+		claims.put("roles", auth.getAuthorities().stream().map(Object::toString).collect(Collectors.toList()));
+		String token = Jwts.builder()
+				.setClaims(claims)
+				.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+				.signWith(SignatureAlgorithm.HS512, secretKey)
+				.compact();
+		res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
+	}
 
-//        System.out.println("Token = " + token);
-        response.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
-    }
 }
