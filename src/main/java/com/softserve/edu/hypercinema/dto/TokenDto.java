a976299a195079e80f0a66aa36ecd03e927a8821
Yaroslav Kuchkovsky <y.kuchkovsky@gmail.com>
2018-05-02 21:41:18 +0300

    UserController, UserService and JwtAuthenticationFilter fixes

---
+++ src/main/java/com/softserve/edu/hypercinema/dto/TokenDto.java
@@ -0,0 +1,12 @@
+package com.softserve.edu.hypercinema.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TokenDto extends BaseDto {
+
+    private String token;
+
+}
