385c2897fe044ea23b963da9fbc707c945a60a38
Anastasiya Didych <didychanastasiya@gmail.com>
2018-05-21 15:59:33 +0300

    uodated SeatConverter

--- src/main/java/com/softserve/edu/hypercinema/controller/SeatController.java
+++ src/main/java/com/softserve/edu/hypercinema/controller/SeatController.java
@@ -20,7 +20,7 @@ public class SeatController {
     private SeatConverter seatConverter;
 
     @GetMapping("/{id}")
-//    @PreAuthorize("hasRole('USER')")
+    @PreAuthorize("hasRole('USER')")
     public SeatDto getSeat(@PathVariable("id") Long id) {
         return seatConverter.convertToDto(seatService.getSeat(id));
     }
